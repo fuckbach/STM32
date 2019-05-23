2d01e7d50246742bb58ee0a97b95dcf6bee1c2c7
fuckbach <nikita.shary@gmail.com>
2019-05-23 17:24:53 +0300

    AddFreeRTOS

--- Lesson2/workspace/Code/src/main.c
+++ Lesson2/workspace/Code/src/main.c
@@ -3,6 +3,8 @@
 #include "task.h"
 #include "queue.h"
 
+void GPIO_Init(void);
+void VTask1(void* argument);
 /*******************************************************************/
 void vApplicationIdleHook( void )
 {
@@ -38,44 +40,37 @@ void vApplicationTickHook( void )
  
 /*******************************************************************/
 
-
- void delay( uint32_t time){
-	 uint32_t i ;
-	 for (i = 0;i<time;i++){
-		 
-	 }
- }
- 
-
- 
  int main(void)
 {
+	GPIO_Init();
+	
+	while(1)
+		{
+	
+	  }
+}
+/******************************************************************/
+
+void GPIO_Init(void){
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
 	GPIOD->MODER |= GPIO_MODER_MODER15_0 | GPIO_MODER_MODER14_0 | GPIO_MODER_MODER13_0 | GPIO_MODER_MODER12_0;
 	GPIOD->OTYPER = 0;
 	GPIOD->OSPEEDR = 0;
+}
 
+void VTask1(void* argument){
 
-	
-	while(1)
-		{
-				GPIOD->BSRR |= GPIO_BSRR_BS_15;
-				delay(2000000);
-				GPIOD->BSRR |= GPIO_BSRR_BR_15;
-				delay(2000000);
-				GPIOD->BSRR |= GPIO_BSRR_BS_14;
-				delay(2000000);
-				GPIOD->BSRR |= GPIO_BSRR_BR_14;
-				delay(2000000);
-				GPIOD->BSRR |= GPIO_BSRR_BS_13;
-				delay(2000000);
-				GPIOD->BSRR |= GPIO_BSRR_BR_13;
-				delay(2000000);	
-				GPIOD->BSRR |= GPIO_BSRR_BS_12;
-				delay(2000000);
-				GPIOD->BSRR |= GPIO_BSRR_BR_12;
-				delay(2000000);
-					
-	  }
+	while(1){
+		GPIOD->BSRR |= GPIO_BSRR_BS_15;
+		GPIOD->BSRR |= GPIO_BSRR_BR_14;
+		vTaskDelay(1000);
+			
+		GPIOD->BSRR |= GPIO_BSRR_BR_15;
+		GPIOD->BSRR |= GPIO_BSRR_BS_14;
+		vTaskDelay(1000);	
+	}
 	
 }
+
+
+
